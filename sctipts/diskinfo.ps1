d062d7cc326a7d1dfc19c0abd2d5c32c5e6baff5
yadac <doruji1982@gmail.com>
2017-10-19 23:02:40 +0900

    mva lesson

---
+++ sctipts/diskinfo.ps1
@@ -0,0 +1,17 @@
+﻿function Get-DiskInfo{
+
+    [cmdletbinding()]
+    param(
+        [parameter(mandatory=$true)]
+        [string]$ComputerName=$env:COMPUTERNAME,
+        [string]$Drive='c:'
+    )
+
+    Get-WmiObject -Class win32_logicaldisk -Filter "deviceid='$Drive'" -ComputerName $ComputerName | 
+    select pscomputername, deviceid, 
+        @{n='Size(MB)';e={$_.size / 1mb -as [int]}},
+        @{n='Free(MB)';e={$_.freespace / 1mb -as [int]}}
+}
+
+function Get-JasonFun1 {Write-Output "PowerShell is fun!"}
+function Get-JasonFun2 {Write-Output "PowerShell is REALLY fun!"}
